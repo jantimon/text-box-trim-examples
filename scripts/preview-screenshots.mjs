357d6c1a8a40f7c9ef789456530717407c2d479b
Jan Nicklas <j.nicklas@me.com>
2022-11-14 11:38:08 +0100

    add examples

--- scripts/preview-screenshots.mjs
+++ scripts/preview-screenshots.mjs
@@ -16,7 +16,12 @@ const rootDir = resolve(dirname(new URL(import.meta.url).pathname), "..");
 const readme = await readFile(resolve(rootDir, 'README.md'), 'utf-8');
 
 // remove preview images for links
-const readmeWithoutImages = readme.replace(/!\[[^\]]*\]\(preview-[^)]*\)/g, '');
+const readmeWithoutImages = readme.replace(/!\[([^\]]*)\]\(preview-[^)]*\)/g, (match, alt) => {
+    if (ignored.some((ignoredExample) => alt.includes(ignoredExample))) {
+        return match;
+    }
+    return "";
+});
 
 // find all links
 const links = readmeWithoutImages.matchAll(/\[([^\]]*)\]\((https:\/\/leading-trim.vercel.app\/\?c=[^)]*)\)/g);
